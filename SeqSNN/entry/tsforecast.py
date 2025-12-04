65a95eb1bd8feebeb6a595a93258bf4a1dc7c46f
Liu Bo <49645739+liubolab@users.noreply.github.com>
2025-12-04 20:10:46 +0800

    Add files via upload

---
+++ SeqSNN/entry/tsforecast.py
@@ -0,0 +1,54 @@
+import warnings
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+from utilsd import get_output_dir, get_checkpoint_dir, setup_experiment
+from utilsd.experiment import print_config
+from utilsd.config import PythonConfig, RegistryConfig, RuntimeConfig, configclass
+
+from SeqSNN.dataset import DATASETS
+from SeqSNN.runner import RUNNERS
+from SeqSNN.network import NETWORKS
+
+warnings.filterwarnings("ignore")
+
+
+@configclass
+class SeqSNNConfig(PythonConfig):
+    data: RegistryConfig[DATASETS]
+    network: RegistryConfig[NETWORKS]
+    runner: RegistryConfig[RUNNERS]
+    runtime: RuntimeConfig = RuntimeConfig()
+
+
+def run_train(config):
+    setup_experiment(config.runtime)
+    print_config(config)
+    trainset = config.data.build(dataset_name="train")
+    validset = config.data.build(dataset_name="valid")
+    testset = config.data.build(dataset_name="test")
+    network = config.network.build(
+        input_size=trainset.num_variables, max_length=trainset.max_seq_len
+    )
+    runner = config.runner.build(
+        network=network,
+        output_dir=get_output_dir(),
+        checkpoint_dir=get_checkpoint_dir(),
+        out_size=config.runner.out_size or trainset.num_classes,
+    )
+    runner.fit(trainset, validset, testset)
+    runner.predict(trainset, "train")
+    runner.predict(validset, "valid")
+    runner.predict(testset, "test")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        default_exp = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), "../../exp/forecast/ispikformer/ispikformer_electricity.yml"
+        ))
+        sys.argv.append(default_exp)
+    
+    _config = SeqSNNConfig.fromcli()
+    run_train(_config)
